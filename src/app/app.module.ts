950a64ae28d2098a1d045e92acd30f75d4b18e9f
raopg <prateek.rao.08@gmail.com>
2020-10-22 18:39:14 -0700

    Fixed root module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, UserProfileComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/services/user/user-service.service.ts
+++ src/app/services/user/user-service.service.ts
@@ -7,7 +7,7 @@ import { User } from '../../models/User';
 })
 export class UserService {
   constructor(private http: HttpClient) {}
-  USER_INFO_URL: string = 'https://localhost:4200/assets/user.json';
+  USER_INFO_URL: string = 'http://localhost:4200/assets/user.json';
   getUserInfo = () => {
     return this.http.get<User>(this.USER_INFO_URL);
   };
