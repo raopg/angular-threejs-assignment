93174f2d1599bf5511a0a7ae540318fa53f544b2
raopg <prateek.rao.08@gmail.com>
2020-10-22 19:43:51 -0700

    Added map coordinates

--- src/app/components/user-map/user-map.component.ts
+++ src/app/components/user-map/user-map.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+import { Control, defaults as defaultControls } from 'ol/control';
 
 @Component({
   selector: 'app-user-map',
+  inputs: ['coordinates'],
   templateUrl: './user-map.component.html',
-  styleUrls: ['./user-map.component.scss']
+  styleUrls: ['./user-map.component.scss'],
 })
 export class UserMapComponent implements OnInit {
+  @Input('coordinates') coordinates: string;
+  map: Map;
+  lat: string;
+  long: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    /* Parse lat and long */
+    [this.lat, this.long] = this.coordinates.trim().split(',');
 
+    this.map = new Map({
+      target: 'map',
+      controls: defaultControls({ attribution: false }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: olProj.fromLonLat([+this.long, +this.lat]),
+        zoom: 5,
+      }),
+    });
+  }
 }
