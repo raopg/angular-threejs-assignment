8aba3b5b076a7b1ed5d643018a45e6d092ec6be2
raopg <prateek.rao.08@gmail.com>
2020-10-24 15:19:47 -0700

    Added obj and material loader code

--- src/app/components/mining-site/mining-site.component.ts
+++ src/app/components/mining-site/mining-site.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-mining-site',
@@ -12,21 +15,71 @@ export class MiningSiteComponent implements OnInit {
   ngOnInit(): void {
     // Add scene and camera
 
-    // Make the renderer and add it's dom element to document body
-    const canvasElement: HTMLCanvasElement = document.querySelector(
-      '#mining-site-canvas'
-    );
-    const renderer = new THREE.WebGLRenderer({ canvas: canvasElement });
-
-    //Load obj
-
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color('lightgrey');
+
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera.position.z = 200;
+
+    // Create OrbitControls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
+
+    //Enable lighting
+    var keyLight = new THREE.DirectionalLight(
+      new THREE.Color('hsl(30, 100%, 75%)'),
+      1.0
+    );
+    keyLight.position.set(-100, 0, 100);
+
+    var fillLight = new THREE.DirectionalLight(
+      new THREE.Color('hsl(240, 100%, 75%)'),
+      0.75
+    );
+    fillLight.position.set(100, 0, 100);
+
+    var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+    backLight.position.set(100, 0, -100).normalize();
+
+    scene.add(keyLight);
+    scene.add(fillLight);
+    scene.add(backLight);
+
+    // Load material and obj
+    const mtlLoader = new MTLLoader();
+    mtlLoader.setPath('../../../assets/threejs-assets/');
+    mtlLoader.load('geomodel.mtl', (materials) => {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.setPath('../../../assets/threejs-assets/');
+      objLoader.load('geomodel.obj', (object) => {
+        object.position.y -= 60;
+        scene.add(object);
+      });
+    });
+
+    // Create basic animation function
+
+    var animate = () => {
+      requestAnimationFrame(animate);
+
+      controls.update();
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
   }
 }
